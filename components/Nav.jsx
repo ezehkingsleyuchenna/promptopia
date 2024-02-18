85e6ad5ba2029cc35f687363180c061e1db777ef
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-02-18 16:49:04 +0100

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -16,7 +18,16 @@ const Nav = () => {
          height={30}
          className="object-contain"
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
+      {/* Mobile */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+
+          </div>
+        ) : <></>}
+      </div>
     </nav>
   )
 }
